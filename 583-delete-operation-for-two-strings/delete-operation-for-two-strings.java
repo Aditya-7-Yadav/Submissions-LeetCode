0b698033f8e6c42697c737418231b0f98f79093c
Aditya <anilyadav3241@gmail.com>
2026-01-17 01:34:19 +0530

    Time: 7 ms (71.35%) | Memory: 46.5 MB (37.40%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+       int n=s1.length();
+        int m=s2.length();
+        int[][] dp= new int[n][m];
+        for(int i=0;i<n;i++)for(int j=0;j<m;j++)dp[i][j]=-1;
+        return n+m-2*trav(s1,s2,n-1,m-1,dp);
+    }
+    public static int trav(String s1, String s2,int n,int m,int[][] dp){
+        if(n==-1||m==-1)return 0;
+        if(dp[n][m]!=-1)return dp[n][m];
+        if(s1.charAt(n)==s2.charAt(m))return dp[n][m]=1+trav(s1,s2,n-1,m-1,dp);
+        return dp[n][m]=Math.max(trav(s1,s2,n,m-1,dp),trav(s1,s2,n-1,m,dp));
+    }
+}
