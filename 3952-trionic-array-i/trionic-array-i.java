188c2b8bc2b07242224e26b10f2cb1758f9a6638
Aditya <anilyadav3241@gmail.com>
2026-02-03 20:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (31.09%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -3,8 +3,10 @@ class Solution {
         int i=0,count=0;
         while(i<nums.length-1 && nums[i]<nums[i+1])i++;
         if(i>0)count++;
+        else return false;
         while(i<nums.length-1 && nums[i]>nums[i+1])i++;
         if(i<nums.length-1)count++;
+        else return false;
         while(i<nums.length-1 && nums[i]<nums[i+1])i++;
         if(i==nums.length-1)count++;
         return count==3;
