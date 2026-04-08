e7be9db3b43b15289ef0d03d1613a5f2f602f6ce
Aditya <anilyadav3241@gmail.com>
2026-04-08 20:30:08 +0530

    Time: 69 ms (6.67%) | Memory: 47.7 MB (78.67%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -3,7 +3,8 @@ class Solution {
         long ans=0;
         for(int i=0;i<q.length;i++){
             for(int j=q[i][0];j<=q[i][1];j+=q[i][2]){
-                nums[j]=(int)((nums[j]*(long)q[i][3])%1000000007L);
+                long num=(nums[j]*(long)q[i][3])%1000000007L;
+                nums[j]=(int)num;
             }
         }
         for(int i=0;i<nums.length;i++)ans^=nums[i];
