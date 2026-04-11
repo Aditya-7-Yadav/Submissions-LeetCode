2304f1ae7070aea513e0de16fa0b0b6b50234733
Aditya <anilyadav3241@gmail.com>
2026-04-12 00:05:53 +0530

    Time: 1232 ms (5.04%) | Memory: 43.1 MB (30.66%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    int num=0;
+    public String getPermutation(int n, int k) {
+        num=k-1;
+        String[] ans= new String[n];
+        String[] anss=new String[1];
+        for(int i=0;i<n;i++)ans[i]=(i+1)+"";
+        boolean[] vis=new boolean[n];
+        StringBuilder sb= new StringBuilder();
+        trav(0,k,n,ans,anss,vis,sb);
+        return anss[0];
+    }
+    void trav(int i,int k,int n,String[] ans,String[] anss,boolean[] vis,StringBuilder sb){
+        // if(num==0)return;
+        if(i==n){
+            num--;
+            if(num==-1)anss[0]=sb.toString();
+            return;
+        }
+        for(int ii=0;ii<n;ii++){
+            if(!vis[ii]){
+                vis[ii]=true;
+                sb.append(ans[ii]);
+                trav(i+1,k,n,ans,anss,vis,sb);
+                vis[ii]=false;
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+}
