dcec7aee3ffbc1520de135aec76dd78c85ffd823
Aditya <anilyadav3241@gmail.com>
2026-07-24 20:42:00 +0530

    Time: 27 ms (14.44%) | Memory: 42.3 MB (39.04%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minSteps(int n) {
         if(n==1)return 0;
-        int ans=1,curr=1,cpy=1;
         boolean cp=true;
-        return trav(ans,curr,n,cpy,cp);
+        int[] dp= new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            dp[i]=9999;
+            for(int j=i-1;j>0;j--){
+                if(i%j==0)
+            dp[i]=Math.min(dp[i],dp[j]+i/j);
+    
+            }
+        }
+        return dp[n];
     }
-    public int trav(int ans,int curr,int n,int cpy,boolean ncpy){
-        if(curr>n)return 99999;
-        if(curr==n)return ans;
-        int c1= trav(ans+1,curr+cpy,n,cpy,false);
-        int c2=999999;
-        //int c3=trav(ans+1,curr)
-        if(!ncpy)c2=trav(ans+1,curr,n,curr,true);
-        return Math.min(c1,c2);
-    }
+    
 }
